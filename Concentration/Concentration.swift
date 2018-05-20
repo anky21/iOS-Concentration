3fd8c4947683de753c6ecfadb110c214298b7d26
Anky An <anky25@gmail.com>
2018-05-20 21:58:15 +1000

    Initial setup of the concentration game

---
+++ Concentration/Concentration.swift
@@ -0,0 +1,48 @@
+//
+//  Concentration.swift
+//  Concentration
+//
+//  Created by Anky An on 20/5/18.
+//  Copyright © 2018 Anky An. All rights reserved.
+//
+
+import Foundation
+
+class Concentration {
+    var cards = [Card]()
+    
+    var indexOfOneAndOnlyFaceUpCard: Int?
+    
+    func chooseCard(at index: Int) {
+        
+        if !cards[index].isMatched {
+            if let matchIndex = indexOfOneAndOnlyFaceUpCard, matchIndex != index {
+                if cards[matchIndex].identifier == cards[index].identifier {
+                    cards[matchIndex].isMatched = true
+                    cards[index].isMatched = true
+                }
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = nil
+                
+            } else {
+                for flipDownIndex in cards.indices {
+                    cards[flipDownIndex].isFaceUp = false
+                }
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = index
+            }
+        }
+    }
+    
+    init(numberOfPairsOfCards: Int) {
+        for _ in 0..<numberOfPairsOfCards {
+            let card = Card()
+            cards.append(card)
+            cards.append(card)
+            
+            // cards += [card, card]
+        }
+        
+        // ToDo: Shuffle the cards
+    }
+}
